a4bc8cfb784063ec91790dda3a457670ca1f4f8d
Blazuti <gustavo.blazuti@hotmail.com>
2026-03-25 10:11:24 -0300

    atualizado

--- lading-page/src/components/works/works.jsx
+++ lading-page/src/components/works/works.jsx
@@ -3,8 +3,9 @@ import Orelha001 from "../media/imgs/piercing_orelha.png";
 import Orelha002 from "../media/imgs/piercing_orelha002.png";
 import style from './works.module.css'
 
+
 export default function works() {
-  return (
+    return (
     <div className={style.works}>
       <div className={style.contain_works}>
         <h1>My Works</h1>
